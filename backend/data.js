feee29fcd8310c72c5d0e202cebd41194d16514a
alibanyali-aspire <ABanyAli@aspire-services.net>
2021-04-11 23:20:58 +0300

    second time

--- backend/data.js
+++ backend/data.js
@@ -1,12 +1,28 @@
+import bcrypt from 'bcrypt';
 const data = {
+    users: [
+        {
+            name: 'AliBanyAli',
+            email: 'banyali@bk.ru',
+            password: bcrypt.hashSync('12345', 8),
+            isAdmin: true,
+
+        }, {
+            name: 'HasanSaimeh',
+            email: 'hasan@gmail.com',
+            password: bcrypt.hashSync('11111', 8),
+            isAdmin: false,
+
+        }
+    ],
     products: [
         {
-            _id: '1',
+
             name: 'Men Formal Dress 1',
             category: 'Clothes',
             image: '/images/1.jpg',
             price: 200,
-            countInStock: 1,
+            countInStock: 10,
             brand: 'Lacoste',
             rating: 4.5,
             numReviews: 10,
@@ -14,12 +30,12 @@ const data = {
 
         },
         {
-            _id: '2',
+
             name: 'Men Formal Dress 2',
             category: 'Clothes',
             image: '/images/2.jpg',
             price: 350,
-            countInStock: 2,
+            countInStock: 11,
             brand: 'Nike',
             rating: 5,
             numReviews: 15,
@@ -27,7 +43,7 @@ const data = {
 
         },
         {
-            _id: '3',
+
             name: ' Men Formal Dress 3',
             category: 'Clothes',
             image: '/images/3.jpg',
@@ -40,12 +56,12 @@ const data = {
 
         },
         {
-            _id: '4',
+
             name: 'Men Formal Dress 4',
             category: 'Clothes',
             image: '/images/1.jpg',
             price: 250,
-            countInStock: 1,
+            countInStock: 3,
             brand: 'adidas',
             rating: 4,
             numReviews: 20,
@@ -53,12 +69,12 @@ const data = {
 
         },
         {
-            _id: '5',
+
             name: 'Men Formal Dress 5',
             category: 'Clothes',
             image: '/images/1.jpg',
             price: 220,
-            countInStock: 1,
+            countInStock: 4,
             brand: 'Puma',
             rating: 3.5,
             numReviews: 22,
@@ -66,7 +82,7 @@ const data = {
 
         },
         {
-            _id: '6',
+
             name: 'Men Formal Dress 6',
             category: 'Clothes',
             image: '/images/1.jpg',
